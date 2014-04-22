29fa09bdc75b0e18b8911669be90675dc7af1ffe
bonakid <jimbertamante@gmail.com>
2014-04-22 14:40:33 +0800

    updating all

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.onload = function(){
+	var name = document.getElementById("name");
 	var flappy = document.getElementById("flappy");
 	var floor = document.getElementById("floor");
 	var pipe1 = document.getElementById("pipe");
@@ -7,11 +8,44 @@ window.onload = function(){
 	var pl = document.getElementById("pl");
 	var mushroom = document.getElementById("mushroom");
 	var mushroom2 = document.getElementById("mushroom2");
+	var mushcover = document.getElementById("mushcover");
 	var bugs = document.getElementsByClassName("pl-bug");
+	var cloud1 = document.getElementById("cloud-cluster1");
+	var cloud2 = document.getElementById("cloud-cluster2");
+	var coin1 = document.getElementById("coin1"),
+		dir1 = document.getElementById("dir1"),
+		dir2 = document.getElementById("dir2"),
+		curtain = document.getElementById("black-curtain"),
+		close = document.getElementById("message-close"),
+		mess = document.getElementById("message-holder"),
+		count = 0;
+	var interval;
 	
-	this.onkeypress = function(e){
-		
-		try{
+	stop();
+
+	window.onkeydown = function(e){
+		name.style.animationPlayState = "running";
+		name.style.webkitAnimationPlayState = "running";
+		if(e.keyCode == 32){
+			go();
+			direction("normal");
+		}else if(e.keyCode == 66){
+			go();
+			direction("reverse");
+		}
+		/*interval = setInterval(function(){
+			console.log(count);
+			count++;
+		},1000);*/
+	}
+	
+	this.onkeyup = function(e){
+		//clearInterval(interval);
+		stop();	
+	}
+
+	function go(){
+
 			flappy.style.animationPlayState = "running";
 			flappy.style.webkitAnimationPlayState = "running";
 			floor.style.animationPlayState = "running";
@@ -28,17 +62,27 @@ window.onload = function(){
 			mushroom.style.webkitAnimationPlayState = "running";
 			mushroom2.style.animationPlayState = "running";
 			mushroom2.style.webkitAnimationPlayState = "running";
+			cloud1.style.animationPlayState = "running";
+			cloud2.style.animationPlayState = "running";
+			cloud1.style.animationPlayState = "running";
+			cloud2.style.animationPlayState = "running";
+			cloud1.style.webkitAnimationPlayState = "running";
+			cloud2.style.webkitAnimationPlayState = "running";
+			coin1.style.animationPlayState = "running";
+			coin1.style.webkitAnimationPlayState = "running";
+			
+			dir1.style.animationPlayState = "running";
+			dir1.style.webkitAnimationPlayState = "running";
+			dir2.style.animationPlayState = "running";
+			dir2.style.webkitAnimationPlayState = "running";
+
 			for(var i = 0;i < bugs.length;i++){
 				bugs[i].style.animationPlayState = "running";
 				bugs[i].style.webkitAnimationPlayState = "running";
 			}
-		}catch(e){
-			alert(e);
-		}
 	}
-	
-	this.onkeyup = function(e){
-		
+
+	function stop(){
 		setTimeout(function(){
 			flappy.style.animationPlayState = "paused";
 			flappy.style.webkitAnimationPlayState = "paused";
@@ -56,6 +100,17 @@ window.onload = function(){
 			mushroom.style.webkitAnimationPlayState = "paused";
 			mushroom2.style.animationPlayState = "paused";
 			mushroom2.style.webkitAnimationPlayState = "paused";
+			cloud1.style.animationPlayState = "paused";
+			cloud1.style.webkitAnimationPlayState = "paused";
+			cloud2.style.animationPlayState = "paused";
+			cloud2.style.webkitAnimationPlayState = "paused";
+			coin1.style.animationPlayState = "paused";
+			coin1.style.webkitAnimationPlayState = "paused";
+			
+			dir1.style.animationPlayState = "paused";
+			dir1.style.webkitAnimationPlayState = "paused";
+			dir2.style.animationPlayState = "paused";
+			dir2.style.webkitAnimationPlayState = "paused";
 			
 			for(var i = 0;i < bugs.length;i++){
 				bugs[i].style.animationPlayState = "paused";
@@ -63,9 +118,62 @@ window.onload = function(){
 			}
 		},100);
 	}
+
+
+	function direction(move){
+			flappy.style.animationDirection = move;
+			flappy.style.webkitAnimationDirection = move;
+			floor.style.animationDirection = move;
+			floor.style.webkitAnimationDirection = move;
+			pipe1.style.animationDirection = move;
+			pipe1.style.webkitAnimationDirection = move;
+			pipe2.style.animationDirection = move;
+			pipe2.style.webkitAnimationDirection = move;
+			bar.style.animationDirection = move;
+			bar.style.webkitAnimationDirection = move;
+			pl.style.animationDirection = move;
+			pl.style.webkitAnimationDirection = move;
+			mushroom.style.animationDirection = move;
+			mushroom.style.webkitAnimationDirection = move;
+			mushroom.style.animationDirection = move;
+			mushroom.style.webkitAnimationDirection = move;
+			mushroom2.style.animationDirection = move;
+			mushroom2.style.webkitAnimationDirection = move;
+			coin1.style.animationDirection = move;
+			coin1.style.webkitAnimationDirection = move;
+			dir1.style.animationDirection = move;
+			dir1.style.webkitAnimationDirection = move;
+			dir2.style.animationDirection = move;
+			dir2.style.webkitAnimationDirection = move;
+			
+			for(var i = 0;i < bugs.length;i++){
+				bugs[i].style.animationDirection = move;
+				bugs[i].style.webkitAnimationDirection = move;
+			}
+	}
+
+	coin1.onclick = function(){
+		mess.innerHTML = "<br>root@bonakid:~# whoami<br><img id='me' src='img/me.png'><br><span class='bold'>Jim-Bert Amante</span><br>Bachelor of Science in Information Technology"
+		curtain.style.animation = "fadein 1s forwards";
+		curtain.style.webkitAnimation = "fadein 1s forwards";
+	}
+	mushcover.onclick = function(){
+		mess.innerHTML = "<br>root@bonakid:~# whoami<br><img id='me' src='img/me.png'><br><span class='bold'>Jim-Bert Amante</span><br>Bachelor of Science in Information Technology"
+		curtain.style.animation = "fadein 1s forwards";
+		curtain.style.webkitAnimation = "fadein 1s forwards";
+	}
+
+	close.onclick = function(){
+		curtain.style.animation = "fadeout1 1s forwards";
+		curtain.style.webkitAnimation = "fadeout1 1s forwards";
+		setTimeout(function(){
+			curtain.style.animation = "";
+			curtain.style.webkitAnimation = "";
+		},1000);
+	}
 	
 }
 
-window.onerror = function(){
-		alert("something wrong");
+window.onerror = function(e){
+		console.log(e);
 	}
